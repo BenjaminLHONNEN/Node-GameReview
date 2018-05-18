1488215b900d661db40528f53aeb3abeed2895a9
BenjaminLHONNEN <benjaminlhonnen@gmail.com>
2018-05-18 10:25:28 +0200

    transfert git repo

---
+++ index.js
@@ -0,0 +1,361 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const mysql2 = require('mysql2');
+const Sequelize = require('sequelize');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const bcrypt = require('bcrypt');
+
+const saltRounds = 12;
+const COOKIE_SECRET = 'le mage noir';
+
+
+app.set('view engine', 'pug');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+app.use(cookieParser(COOKIE_SECRET));
+// Keep track of user sessions
+app.use(session({
+    secret: COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
+
+// Initialize passport, it must come after Express' session() middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app.get('/', (req, res) => {
+    if (req.user !== undefined && req.user !== null) {
+        console.log(req.user);
+        res.render('index', {user: req.user.dataValues});
+    } else {
+        res.redirect("/login");
+    }
+});
+
+
+app.get('/addReview', (req, res) => {
+    if (req.user !== undefined && req.user !== null) {
+        console.log(req.user);
+        res.render('addReview', {user: req.user.dataValues});
+    } else {
+        res.redirect("/login");
+    }
+});
+app.get('/disconnect', (req, res) => {
+    req.user = null;
+    res.redirect('login');
+});
+
+
+app.get('/api/getAllReviews', (req, res) => {
+    if (req.user !== undefined && req.user !== null) {
+        let resp = [];
+        Review
+            .sync()
+            .then(() => {
+                return Review.findAll({
+                    include: [
+                        {
+                            model: Comment,
+                            include: User,
+                        }
+                    ]
+                });
+            })
+            .then((articles) => {
+                articles.forEach(async function (value, index, array) {
+                    let obj = array[index].dataValues;
+                    await DownVote
+                        .count({where: {idReview: obj.id}})
+                        .then(function (downVoteCount) {
+                            obj['downVoteCount'] = downVoteCount;
+                        });
+                    await DownVote
+                        .find({
+                            where: {
+                                idReview: obj.id,
+                                idUser: req.user.id
+                            }
+                        })
+                        .then(function (upVote) {
+                            obj['voteDown'] = upVote;
+                        });
+
+                    await UpVote
+                        .count({where: {idReview: obj.id}})
+                        .then(function (upVoteCount) {
+                            obj['upVoteCount'] = upVoteCount;
+                        });
+                    await UpVote
+                        .find({
+                            where: {
+                                idReview: obj.id,
+                                idUser: req.user.id
+                            }
+                        })
+                        .then(function (downVote) {
+                            obj['voteUp'] = downVote;
+                        });
+
+                    resp.push(obj);
+                    if (index === array.length - 1) {
+                        res.setHeader('Content-Type', 'application/json');
+                        res.send(JSON.stringify(resp));
+                    }
+                });
+            })
+    } else {
+        res.send("403");
+    }
+
+});
+
+app.post('/api/addArticle', (req, res) => {
+    Review
+        .sync()
+        .then(() => {
+            Review.create({
+                NomJeu: req.body.NomJeu,
+                Review: req.body.Review,
+                Note: req.body.Note,
+            });
+        })
+        .then(() => {
+            res.redirect("/");
+        });
+});
+app.get('/api/addUpVote-:idReview', (req, res) => {
+    UpVote
+        .sync()
+        .then(() => {
+            return DownVote.find({
+                where: {
+                    idReview: req.params.idReview,
+                    idUser: req.user.id,
+                }
+            })
+        })
+        .then((downvote) => {
+            if (downvote === null) {
+                return UpVote.find({
+                    where: {
+                        idReview: req.params.idReview,
+                        idUser: req.user.id,
+                    }
+                })
+            } else {
+                DownVote.destroy({
+                    where:{
+                        id: downvote.id
+                    }
+                });
+                return true;
+            }
+        })
+        .then((create) => {
+            if (create === true || create === null) {
+                UpVote.create({
+                    idReview: req.params.idReview,
+                    idUser: req.user.id,
+                });
+                res.send(200);
+            } else {
+                res.send(403);
+            }
+        })
+});
+app.get('/api/addDownVote-:idReview', (req, res) => {
+    DownVote
+        .sync()
+        .then(() => {
+            return UpVote.find({
+                where: {
+                    idReview: req.params.idReview,
+                    idUser: req.user.id,
+                }
+            })
+        })
+        .then((upvote) => {
+            if (upvote === null) {
+                return DownVote.find({
+                    where: {
+                        idReview: req.params.idReview,
+                        idUser: req.user.id,
+                    }
+                })
+            } else {
+                UpVote.destroy({
+                    where: {
+                        id: upvote.id
+                    }
+                });
+                return true;
+            }
+        })
+        .then((create) => {
+            if (create === true || create === null) {
+                DownVote.create({
+                    idReview: req.params.idReview,
+                    idUser: req.user.id,
+                });
+                res.send(200);
+            } else {
+                res.send(403);
+            }
+        })
+});
+app.post('/api/addComment-:idReview', (req, res) => {
+    Comment
+        .sync()
+        .then(() => {
+            Comment.create({
+                idReview: req.params.idReview,
+                idUser: req.user.id,
+                comment: req.body.comment,
+            });
+        })
+        .then(() => {
+            res.send(200);
+        });
+});
+
+app.get('/register', (req, res) => {
+    // Render the login page
+    res.render('register');
+});
+app.post('/register', (req, res) => {
+    bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+        User
+            .sync()
+            .then(() => {
+                User.find({
+                    where: {
+                        mail: req.body.userName,
+                    }
+                })
+            })
+            .then((user) => {
+                if (user === null) {
+                    return User.create({
+                        mail: req.body.userName,
+                        password: hash,
+                        userName: req.body.pseudo,
+                    });
+                }
+            })
+            .then((user) => {
+                user = user.dataValues;
+                console.error(user);
+                req.login(user, function (err) {
+                    if (err) {
+                        console.error(err);
+                        res.redirect('/register');
+                    } else {
+                        res.redirect('/');
+                    }
+                });
+            })
+            .catch(() => {
+                res.send(500);
+            });
+    });
+});
+app.post('/login', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login'
+}));
+
+app.get('/login', (req, res) => {
+    // Render the login page
+    res.render('login');
+});
+
+passport.serializeUser((user, cb) => {
+    cb(null, user.mail);
+});
+passport.deserializeUser((mail, cb) => {
+    // Get a user from a cookie's content: his email
+    User
+        .findOne({where: {mail}})
+        .then((user) => {
+            cb(null, user);
+        })
+        .catch(cb);
+});
+
+passport.use(new LocalStrategy((mail, password, done) => {
+    User
+        .findOne({where: {mail}})
+        .then(function (user) {
+            if (user) {
+                bcrypt.compare(password, user.dataValues.password, function (err, res) {
+                    if (res === true) {
+                        return done(null, user);
+                    } else {
+                        return done(null, false, {
+                            message: 'Invalid credentials'
+                        });
+                    }
+                });
+
+            } else {
+                return done(null, false, {
+                    message: 'Invalid credentials'
+                });
+            }
+        })
+        // If an error occured, report it
+        .catch(done);
+}));
+
+app.listen(3000);
+
+//DataBase
+const db = new Sequelize('gamereview', 'root', '', {
+    host: 'localhost',
+    dialect: 'mysql'
+});
+const Review = db.define('article', {
+    NomJeu: {type: Sequelize.STRING},
+    Review: {type: Sequelize.TEXT},
+    Note: {type: Sequelize.INTEGER},
+});
+const UpVote = db.define('upvote', {
+    idReview: {type: Sequelize.INTEGER},
+    idUser: {type: Sequelize.INTEGER},
+});
+const DownVote = db.define('downvote', {
+    idReview: {type: Sequelize.INTEGER},
+    idUser: {type: Sequelize.INTEGER},
+});
+const Comment = db.define('comment', {
+    idReview: {type: Sequelize.INTEGER},
+    idUser: {type: Sequelize.INTEGER},
+    comment: {type: Sequelize.TEXT},
+});
+
+const User = db.define('user', {
+    mail: {type: Sequelize.STRING},
+    password: {type: Sequelize.STRING},
+    userName: {type: Sequelize.STRING},
+});
+
+Review.hasMany(Comment, {foreignKey: 'idReview'});
+Comment.belongsTo(Review, {foreignKey: 'idReview'});
+User.hasMany(Comment, {foreignKey: 'idUser'});
+Comment.belongsTo(User, {foreignKey: 'idUser'});
+
+User.sync();
+UpVote.sync();
+DownVote.sync();
+Comment.sync();
+Review.sync();
+
